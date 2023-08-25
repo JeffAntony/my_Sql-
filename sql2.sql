19de1a94b63404ec0cb863a409254d52168be16c
JeffAntony <142667486+JeffAntony@users.noreply.github.com>
2023-08-25 13:42:56 +0530

    Update sql2.sql

--- sql2.sql
+++ sql2.sql
@@ -2,11 +2,13 @@
 show databases;
 
 use sakila;
+select * from film;
 
-select  title,film_id,release_year,description from file where title like 'a%an';
-select  title,film_id,release_year,description from file where title like '%t_';
-select  title,film_id,release_year,description from file where title like '_f_i%egg%';
-select  title,film_id,release_year,description from file where title like '_a_';
+select  title,film_id,release_year,description from film where title like 'a%an';
+select  title,film_id,release_year,description from film where title like '%t_';
+select  title,film_id,release_year,description from film where title like '_f_i%egg%';
+select  title,film_id,release_year,description from film where title like '_a_';
+select title,film_id,release_year,description from film where title like '%t____%;
 
 
 
